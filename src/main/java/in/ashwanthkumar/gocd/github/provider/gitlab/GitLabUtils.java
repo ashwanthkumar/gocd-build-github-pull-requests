4ee44924e80695810a8583e1fc0517b764abcf37
Ahmadou Diouf <ahmadou.diouf@gmail.com>
2020-11-06 17:58:01 +0100

    fix(Gitlab) : fixed the repository url parsing to account for multi groups projects

--- src/main/java/in/ashwanthkumar/gocd/github/provider/gitlab/GitLabUtils.java
+++ src/main/java/in/ashwanthkumar/gocd/github/provider/gitlab/GitLabUtils.java
@@ -1,16 +1,33 @@
 package in.ashwanthkumar.gocd.github.provider.gitlab;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class GitLabUtils {
     public static final String HTTP_PROTOCOL = "http";
     public static final String HTTPS_PROTOCOL = "https";
     public static final String URL_SEPARATOR = "/";
+    public static final String GIT_URL_EXTENSION = ".git";
+    public static final int GIT_URL_EXTENSION_LENGTH = 4;
+    /**
+     * After http url split with url separator a simple project will have no more than 5 splits
+     */
+    public static final int SIMPLE_PROJECT_HTTP_URL_PART_SIZE = 5;
+    /**
+     * After http url split index value where we find the first part of repository path
+     */
+    public static final int URL_WITH_PROTOCOL_START_INDEX = 3;
+    public static final String AT_CHARACTER = "@";
+    public static final int REPOSITORY_URL_INDEX = 1;
+    public static final String COLUMN = ":";
+    public static final int REPOSITORY_PATH_INDEX = 1;
+    public static final int URL_WITHOUT_PROTOCOL_START_INDEX = 1;
 
     public static Properties readPropertyFile() throws IOException {
         File propertyFile = new File(System.getProperty("user.home"), ".gitlab");
@@ -36,6 +53,55 @@ public class GitLabUtils {
             return HTTP_PROTOCOL;
         }
     }
+
+    private static boolean isAuthorityPresentInURI(String url) {
+        return url.contains(AT_CHARACTER) ;
+    }
+    /**
+     * Will extract the project path from the repository url.
+     * @param url
+     * @return
+     */
+    public static String getProjectPathFromUrl(String url) {
+        String normalizedUrl = getNormalizedUrl(url);
+        String projectPath = "";
+        if (isAuthorityPresentInURI(url)) {
+            projectPath = getProjectPathFromUriWithAuthorithy(normalizedUrl);
+        } else {
+            projectPath = getProjectPathFromUriWithoutAuthorithy(normalizedUrl,URL_WITH_PROTOCOL_START_INDEX);
+        }
+        return removeGitExtension(projectPath);
+    }
+
+    private static String getNormalizedUrl(String url) {
+        url = StringUtils.trim(url);
+        if(url.endsWith(URL_SEPARATOR)) {
+            url = url.substring(0, url.length() - 1);
+        }
+        return url.toLowerCase();
+    }
+
+    private static String getProjectPathFromUriWithAuthorithy(String normalizedUrl) {
+        String projectPath = "";
+        String repositoryUrlWithoutAuthority = normalizedUrl.split(AT_CHARACTER)[REPOSITORY_URL_INDEX];
+        if(repositoryUrlWithoutAuthority.contains(COLUMN)) {
+            projectPath = repositoryUrlWithoutAuthority.split(COLUMN)[REPOSITORY_PATH_INDEX];
+        } else {
+            projectPath = getProjectPathFromUriWithoutAuthorithy(repositoryUrlWithoutAuthority, URL_WITHOUT_PROTOCOL_START_INDEX);
+        }
+        return projectPath;
+    }
+
+    private static String removeGitExtension(String projectPath) {
+        if (projectPath.endsWith(GIT_URL_EXTENSION)) return projectPath.substring(0, projectPath.length() - GIT_URL_EXTENSION_LENGTH);
+        else return projectPath;
+    }
+
+    private static String getProjectPathFromUriWithoutAuthorithy(String normalizedUrl, int startIndex) {
+        String[] urlParts = normalizedUrl.split(URL_SEPARATOR);
+        return String.join(URL_SEPARATOR, Arrays.copyOfRange(urlParts, startIndex,urlParts.length));
+    }
+
 
     public static String getServerUrl(String url) {
         if(isSSHUrl(url)) {
--- src/test/java/in/ashwanthkumar/gocd/github/provider/gitlab/GitLabUtilsTest.java
+++ src/test/java/in/ashwanthkumar/gocd/github/provider/gitlab/GitLabUtilsTest.java
@@ -3,24 +3,43 @@ package in.ashwanthkumar.gocd.github.provider.gitlab;
 import org.junit.Test;
 
 import static in.ashwanthkumar.gocd.github.provider.gitlab.GitLabUtils.*;
+import static in.ashwanthkumar.gocd.github.provider.gitlab.GitLabUtils.getProjectPathFromUrl;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class GitLabUtilsTest {
     @Test
     public void shouldIdentifySSHUrl() {
-        assertThat(isSSHUrl("git@gitlab.com:ashwanthkumar/gocd-build-github-pull-requests.git"), is(true));
-        assertThat(isSSHUrl("https://gitlab.com/ashwanthkumar/gocd-build-github-pull-requests.git"), is(false));
-        assertThat(isSSHUrl("git@Gitlab.Com:ashwanthkumar/gocd-build-github-pull-requests"), is(true));
+        assertThat(isSSHUrl("git@gitlab.com:ashwanthkumar/gocd-build-gitlab-pull-requests.git"), is(true));
+        assertThat(isSSHUrl("https://gitlab.com/ashwanthkumar/gocd-build-gitlab-pull-requests.git"), is(false));
+        assertThat(isSSHUrl("git@Gitlab.Com:ashwanthkumar/gocd-build-gitlab-pull-requests"), is(true));
         assertThat(isSSHUrl("git@code.corp.yourcompany.com:username/repo"), is(true));
     }
 
     @Test
+    public void shouldReturnCorrectProjectPath() {
+        assertEquals("ashwanthkumar/gocd-build-gitlab-pull-requests",getProjectPathFromUrl("https://gitlab.com/ashwanthkumar/gocd-build-gitlab-pull-requests.git"));
+        assertEquals("username/repo",getProjectPathFromUrl("git@code.corp.yourcompany.com:username/repo"));
+        assertEquals("ashwanthkumar/gocd-build-gitlab-pull-requests",getProjectPathFromUrl("https://gitlab.Com/ashwanthkumar/gocd-build-gitlab-pull-requests"));
+        assertEquals("user/test-repo",getProjectPathFromUrl("https://gitlab.company.com/user/test-repo.git"));
+        assertEquals("user/test-repo",getProjectPathFromUrl(" https://gitlab.company.com/user/test-repo.git   "));
+        assertEquals("user/test-repo",getProjectPathFromUrl("https://gitlab.company.com/user/test-repo.git/"));
+
+        //subgroups handling
+        assertEquals("group/subgroup/subgroup2/repo",getProjectPathFromUrl("git@code.corp.yourcompany.com:group/subgroup/subgroup2/repo"));
+        assertEquals("group1/subgroup2/user/test-repo",getProjectPathFromUrl("http://gitlab.company.com/group1/subgroup2/user/test-repo.git"));
+        assertEquals("group1/subgroup2/user/test-repo",getProjectPathFromUrl("https://gitlab.company.com/group1/subgroup2/user/test-repo.git"));
+        assertEquals("path/to/repo",getProjectPathFromUrl("ssh://user@host.xz/path/to/repo.git/"));
+        assertEquals("group1/subgroup2/user/test-repo",getProjectPathFromUrl("https://gitlab.company.com:8956/group1/subgroup2/user/test-repo.git"));
+    }
+
+    @Test
     public void shouldGetServerUrl() {
-        assertThat(getServerUrl("https://gitlab.com/ashwanthkumar/gocd-build-github-pull-requests.git"), is("https://gitlab.com"));
+        assertThat(getServerUrl("https://gitlab.com/ashwanthkumar/gocd-build-gitlab-pull-requests.git"), is("https://gitlab.com"));
         assertThat(getServerUrl("git@code.corp.yourcompany.com:username/repo"), is("https://code.corp.yourcompany.com"));
-        assertThat(getServerUrl("https://Github.Com/ashwanthkumar/gocd-build-github-pull-requests"),
-                is("https://Github.Com"));
+        assertThat(getServerUrl("https://gitlab.Com/ashwanthkumar/gocd-build-gitlab-pull-requests"),
+                is("https://gitlab.Com"));
         assertThat(getServerUrl("https://gitlab.company.com/user/test-repo.git"), is("https://gitlab.company.com"));
     }
 }
