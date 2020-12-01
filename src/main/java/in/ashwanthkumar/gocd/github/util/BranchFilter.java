698f38125c47c66a4cbe2e8f4401ba5c81f4f319
Mehmet Celen <mehmet.celen@gamesys.ee>
2020-12-01 10:42:38 +0200

    fix-typo-branch-filter

--- src/main/java/in/ashwanthkumar/gocd/github/util/BranchFilter.java
+++ src/main/java/in/ashwanthkumar/gocd/github/util/BranchFilter.java
@@ -8,7 +8,7 @@ public class BranchFilter {
 
     public static final String NO_BRANCHES = "";
     private final BranchMatcher blacklistedBranches;
-    private final BranchMatcher whitelistedBrandches;
+    private final BranchMatcher whitelistedBranches;
 
     public BranchFilter() {
         this(NO_BRANCHES, NO_BRANCHES);
@@ -16,15 +16,15 @@ public class BranchFilter {
 
     public BranchFilter(String blacklistOption, String whitelistOption) {
         this.blacklistedBranches = new BranchMatcher(blacklistOption, BranchMatcher.Mode.FAIL_EMPTY);
-        this.whitelistedBrandches = new BranchMatcher(whitelistOption, BranchMatcher.Mode.PASS_EMPTY);
+        this.whitelistedBranches = new BranchMatcher(whitelistOption, BranchMatcher.Mode.PASS_EMPTY);
     }
 
     public boolean isBranchValid(String branch) {
         if (branch == null) {
             return false;
-        } else if (whitelistedBrandches.isEmpty() && blacklistedBranches.isEmpty()) {
+        } else if (whitelistedBranches.isEmpty() && blacklistedBranches.isEmpty()) {
             return true;
-        } else if (whitelistedBrandches.matches(branch) && !blacklistedBranches.matches(branch)) {
+        } else if (whitelistedBranches.matches(branch) && !blacklistedBranches.matches(branch)) {
             return true;
         } else {
             return false;
